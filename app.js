00091a1b99f562e54208c36fa88061b800c24ddc
MWaqasAfridi <153609017+MWaqasAfridi@users.noreply.github.com>
2024-03-27 22:35:08 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// Question no 1
+
+// var arr;
+// arr = [[]];
+
+ 
+// Question no 2
+
+// var arr;
+// arr = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+
+// for(var i = 0; i < arr.length; i++){
+//      document.write(arr[i].join(" ")+"<br>");
+// }
+
+// Question no 3
+
+// var i;
+// for(i = 1; i <= 10; i++){
+//    document.write(i+"<br>");
+// }
+
+
+// Question no 4
+
+// var table = prompt("Enter a number to show its multiplication table");
+// document.write("Multiplication table of " + table + "<br>");
+
+// var tableLength = prompt("Enter length multiplication table");
+// document.write("Length " + tableLength + "<br><br>");
+
+// for(var i = 1; i <= tableLength; i++){
+//     document.write(`${table} x ${i} = ${table*i} <br>`)
+// }
+
+
+// Question no 5
+
+// var fruits = ['apple','banana','mango','orange','strawberry'];
+// for(var i = 0; i < fruits.length; i++){
+// document.write(`${fruits[i]} <br>`)
+// }
+// document.write("<br>")
+// for(var i = 0; i < fruits.length; i++){
+//     document.write(`Element at index ${i} is ${fruits[i]} <br>`)
+//     }
