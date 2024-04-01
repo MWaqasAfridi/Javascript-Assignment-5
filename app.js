64d51e11aa04d06da031c8d2ee725820804c4e27
MWaqasAfridi <153609017+MWaqasAfridi@users.noreply.github.com>
2024-04-01 13:04:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -44,3 +44,93 @@
 // for(var i = 0; i < fruits.length; i++){
 //     document.write(`Element at index ${i} is ${fruits[i]} <br>`)
 //     }
+
+
+// Question no 6
+
+// document.write(`<h3> Counting: </h3>`);
+// for(var i = 1; i <= 15; i++){
+//         document.write(`${i} , `);
+// }
+
+// document.write(`<h3> Reverse Counting: </h3>`);
+// for(var i = 10; i >= 1; i--){
+//         document.write(`${i} , `);
+// }
+
+// document.write(`<h3> Even: </h3>`);
+// for(var i = 0; i <= 20; i++){
+//     if(i % 2 === 0){
+//         document.write(`${i} , `);
+//     }
+// }
+
+// document.write(`<h3> Odd: </h3>`);
+// for(var i = 1; i <= 20; i++){
+//     if(i % 2 !== 0){
+//         document.write(`${i} , `);
+//     }
+// }
+
+// document.write(`<h3> Series: </h3>`);
+// for(var i = 2; i <= 20; i++){
+//     if(i % 2 === 0){
+//         document.write(`${i}k , `);
+//     }
+// }
+
+
+// Question no 7
+
+// var arr = ['cake','apple pie','cookie','chips','patties'];
+
+// var userInp = prompt("Welcome to Waqas Bakery.What do you want to order sir / ma'am");
+// var found = false;
+// for(var i = 0; i < arr.length; i++){
+//    if(arr[i] === userInp){
+//          found = true;
+//          break;
+//    }
+// }
+// if(found){
+//   document.write(`${arr[i]} is <b> available </b> at index ${i} in our bakry`)
+// }
+// else{
+//     document.write(`We are sorry.${userInp} is <b> not available </b> in our bakry`)
+// }
+
+
+// Question no 8
+
+// var arr = [24,53,78,91,12];
+// console.log(`Array items : ${arr}`);
+// var largest = arr[0];
+// for(var i = 0; i < arr.length; i++){
+//         if(arr[i] > largest){
+//              largest = arr[i];
+//         }
+// }
+// console.log(`The largest number is : ${largest}`);
+
+
+// Question no 9
+
+// var arr = [24,53,78,91,12];
+// console.log(`Array items : ${arr}`);
+// var smallest = arr[0];
+// for(var i = 0; i < arr.length; i++){
+//         if(arr[i] < smallest){
+//              smallest = arr[i];
+//         }
+// }
+// console.log(`The smallest number is : ${smallest}`);
+
+
+// Quuestion no 10
+
+// var i;
+// for(i = 1; i <= 100; i++){
+//       if(i % 5 === 0){
+//          document.write(`${i} , `);
+//       }
+// }
